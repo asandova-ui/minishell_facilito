731e8acfb8fa608a8237f3ed00746c889b2e4c31
Javier Morales <101357089+JaviMoraales@users.noreply.github.com>
2024-08-13 10:38:29 +0200

    historial funcional

--- minishell_yepa/src/main.c
+++ minishell_yepa/src/main.c
@@ -9,11 +9,14 @@
 int main(int argc, char **argv, char **envp)
 {
     t_minish mini;
+    t_history *history;
     char *line = NULL;
 
     (void)argc;
     (void)argv;
     init_struct(&mini, envp);
+    history = malloc(sizeof(t_history));
+    init_struct_history(history);
     while (mini.exit == 0)
     {
         init_path(&mini);
@@ -24,13 +27,14 @@ int main(int argc, char **argv, char **envp)
         line = get_next_line(STDIN_FILENO);
         if (line == NULL)
             break; // Handle EOF or error in get_next_line
-        minishell(line, &mini);
+        add_to_history(history, line);
+        minishell(line, &mini, history);
         free(line);
     }
     exit(EXIT_SUCCESS);
 }
 
-void minishell(char *line, t_minish *mini)
+void minishell(char *line, t_minish *mini, t_history *history)
 {
     char *temp;
 
@@ -40,11 +44,31 @@ void minishell(char *line, t_minish *mini)
 
     //ya tienes la linea con control de caracteres especiales y de comillas
     //si el caracter es no reconocible ASCII(-36) significa que es dolar seguido de cosas, es decir toca poner el valor de la variable
-    built_ins(temp, mini);//solo builtins
+    built_ins(temp, mini, history);//solo builtins
     if (mini->exec == 0)
         run_command(line,mini);
     //ahora los comandos requeridos
     mini->exec = 0;
+}
+
+void add_to_history(t_history *history, char *command)
+{
+    int i;
+
+    if (history->count < 1000)
+        history->history[history->count++] = ft_strdup(command);
+    else
+    {
+        free(history->history[0]);
+        i = 1;
+        while (i < 1000)
+        {
+            history->history[i - 1] = history->history[i];
+            i++;
+        }
+        history->history[999] = ft_strdup(command);
+    }
+    history->i = history->count;
 }
 
 void init_struct(t_minish *mini, char **envp)
