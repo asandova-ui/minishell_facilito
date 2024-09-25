b0d7f90daaf1525c3f536a02ad4b4cd28b2abeff
Javier Morales <101357089+JaviMoraales@users.noreply.github.com>
2024-09-25 06:24:49 +0200

    mejoras en builtins

--- minishell_definitiva/src/built_ins/echo/quotes_utils.c
+++ minishell_definitiva/src/built_ins/echo/quotes_utils.c
@@ -6,7 +6,7 @@
 /*   By: jamorale <jamorale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 18:46:01 by jamorale          #+#    #+#             */
-/*   Updated: 2024/09/24 03:03:02 by jamorale         ###   ########.fr       */
+/*   Updated: 2024/09/25 05:36:10 by jamorale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,78 +14,64 @@
 #include "../../../printf/includes/ft_printf.h"
 #include "../../../printf/libft/libft.h"
 
-char	*handle_quotes(const char *str)
+void	update_quote_state(const char *str, t_quote_state *state)
 {
-	char		*ptr;
-	const char	*start = str;
-	int			in_single_quote;
-	int			in_double_quote;
-	size_t		new_len;
-	char		*new_result;
-	char		*result;
-	char		*extra_input;
+	while (*str)
+	{
+		if (*str == '\'' && !state->in_double_quote)
+			state->in_single_quote = !state->in_single_quote;
+		else if (*str == '\"' && !state->in_single_quote)
+			state->in_double_quote = !state->in_double_quote;
+		str++;
+	}
+}
+
+size_t	calculate_new_length(const char *result, const char *extra_input,
+		int is_first_input)
+{
+	size_t	new_len;
+
+	new_len = strlen(result) + strlen(extra_input);
+	if (!is_first_input)
+		new_len += 1;
+	return (new_len + 1);
+}
+
+char	*allocate_new_result(char *result, const char *extra_input,
+		int is_first_input)
+{
+	size_t	new_len;
+	char	*new_result;
+
+	new_len = calculate_new_length(result, extra_input, is_first_input);
+	new_result = realloc(result, new_len);
+	if (!new_result)
+	{
+		perror("realloc");
+		free(result);
+		free((char *)extra_input);
+		exit(EXIT_FAILURE);
+	}
+	return (new_result);
+}
+
+void	append_input(char **result, const char *extra_input, int is_first_input)
+{
+	if (!is_first_input)
+		strcat(*result, "\n");
+	strcat(*result, extra_input);
+}
+
+char	*initialize_result(const char *str)
+{
+	char	*result;
 
 	result = malloc(strlen(str) + 1);
-	if (result == NULL)
+	if (!result)
 	{
 		perror("malloc");
 		exit(EXIT_FAILURE);
 	}
-	ptr = result;
-	in_single_quote = 0;
-	in_double_quote = 0;
-	while (*start)
-	{
-		if (*start == '\'' && !in_double_quote)
-		{
-			in_single_quote = !in_single_quote;
-		}
-		else if (*start == '\"' && !in_single_quote)
-		{
-			in_double_quote = !in_double_quote;
-		}
-		*ptr++ = *start++;
-	}
-	*ptr = '\0';
-	while (in_single_quote || in_double_quote)
-	{
-		printf("quote> ");
-		extra_input = readline(NULL);
-		if (!extra_input)
-		{
-			break ;
-		}
-		new_len = strlen(result) + strlen(extra_input) + 1;
-		new_result = realloc(result, new_len);
-		if (new_result == NULL)
-		{
-			perror("realloc");
-			free(result);
-			free(extra_input);
-			exit(EXIT_FAILURE);
-		}
-		result = new_result;
-		strcat(result, extra_input);
-		free(extra_input);
-		ptr = result;
-		in_single_quote = 0;
-		in_double_quote = 0;
-		while (*ptr)
-		{
-			if (*ptr == '\'' && !in_double_quote)
-			{
-				in_single_quote = !in_single_quote;
-			}
-			else if (*ptr == '\"' && !in_single_quote)
-			{
-				in_double_quote = !in_double_quote;
-			}
-			ptr++;
-		}
-		if (!in_single_quote && !in_double_quote)
-		{
-			break ;
-		}
-	}
+	strcpy(result, str);
 	return (result);
 }
